8c04e17c4b22c599542d1e9177423cde3fce8426
EternalEffy <39722892+EternalEffy@users.noreply.github.com>
2021-02-12 01:34:42 +0300

    помогите пожалуйстаX2

--- src/main.java
+++ src/main.java
@@ -11,12 +11,6 @@ public class main {
         Scanner console = new Scanner(System.in);
         picture.loadPicture(console.nextLine());
 
-        /*for(int x=0;x< picture.returnWidth();x++){
-            for (int y=0;y< picture.returnHeight();y++){
-                picture.changePixel(Color.YELLOW,x,y);
-            }
-        }*/
-
        for(int y=0;y<picture.returnHeight();y++){
             for (int x=0;x< picture.returnWidth();x++)
                 if (picture.colorOfPixel(x, y) == -131072) {
